75d9b09cbee4c3b61380faa5d8f33155ab1fb0c2
Siva Addanki <34810623+avsssai@users.noreply.github.com>
2024-01-13 22:34:32 +0530

    something

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,8 +1,29 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import invariant from "tiny-invariant";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+	const { jokeId } = params;
+	invariant(params.jokeId, "Joke Id missing from params.");
+	const joke = await db.joke.findUnique({
+		where: {
+			id: jokeId,
+		},
+	});
+	if (!SingleJoke) {
+		throw new Error("Joke not found.");
+	}
+	return json({ singleJoke: joke });
+};
+
 export default function SingleJoke() {
+	const { singleJoke } = useLoaderData<typeof loader>();
 	return (
 		<div>
-			<p>What do you call a fish philosophy teaching institute?</p>
-			<p>A school of thought.</p>
+			<p>{singleJoke?.name}</p>
+			<p>{singleJoke?.content}</p>
+			<Link to={`.`}>{singleJoke?.name} Permalink</Link>
 		</div>
 	);
 }
--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,17 +1,62 @@
-import { Outlet } from "@remix-run/react";
-import { LinksFunction } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { LinksFunction, json } from "@remix-run/node";
 import stylesUrl from "../styles/jokes.css";
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => [
 	{ href: stylesUrl, rel: "stylesheet" },
 ];
 
+export const loader = async () => {
+	try {
+		const jokes = await db.joke.findMany({
+			orderBy: { createdAt: "desc" },
+			select: { name: true, id: true },
+			take: 5,
+		});
+		return json({ jokeListItems: jokes });
+	} catch (error) {
+		throw new Response("Not found", { status: 404 });
+	}
+};
+
 export default function Jokes() {
+	const data = useLoaderData<typeof loader>();
 	return (
-		<div>
-			<h1>J🤪KES</h1>
-			<main>
-				<Outlet />
+		<div className='jokes-layout'>
+			<header className='jokes-header'>
+				<div className='container'>
+					<h1 className='home-link'>
+						<Link
+							to='/'
+							title='Remix Jokes'
+							aria-label='Remix Jokes'>
+							<span className='logo'>🤪</span>
+							<span className='logo-medium'>J🤪KES</span>
+						</Link>
+					</h1>
+				</div>
+			</header>
+			<main className='jokes-main'>
+				<div className='container'>
+					<div className='jokes-list'>
+						<Link to='.'>Get a random joke</Link>
+						<p>Here are a few more jokes to check out:</p>
+						<ul>
+							{data.jokeListItems.map(({ id, name }) => (
+								<li key={id}>
+									<Link to={id}>{name}</Link>
+								</li>
+							))}
+						</ul>
+						<Link to='new' className='button'>
+							Add your own
+						</Link>
+					</div>
+					<div className='jokes-outlet'>
+						<Outlet />
+					</div>
+				</div>
 			</main>
 		</div>
 	);
