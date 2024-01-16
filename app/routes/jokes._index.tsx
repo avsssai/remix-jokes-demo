8d7f99209a5425bd7f4c115432c1f42a927ecc3d
Siva Addanki <34810623+avsssai@users.noreply.github.com>
2024-01-16 12:59:20 +0530

    completed without auth

--- app/routes/jokes._index.tsx
+++ app/routes/jokes._index.tsx
@@ -1,8 +1,27 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async (params: LoaderFunctionArgs) => {
+	const count = await db.joke.count();
+	const random = Math.floor(Math.random() * count);
+	const [randomJoke] = await db.joke.findMany({
+		skip: random,
+		take: 1,
+	});
+	return json({ joke: randomJoke });
+};
+
 export default function Index() {
+	const { joke } = useLoaderData<typeof loader>();
+
 	return (
 		<div>
-			<p>What has 3 legs but no hands?</p>
-			<p>A Tripod stand</p>
+			<p>{joke.name}</p>
+			<p>{joke.content}</p>
+			<p>
+				<Link to={`/jokes/${joke.id}`}>{joke.name} Permalink</Link>
+			</p>
 		</div>
 	);
 }
--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -1,22 +1,118 @@
+import { ActionFunctionArgs, json, redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import { badRequest } from "~/utils/request.server";
+
+function validateJokeContent(content: string) {
+	if (content.length < 3) {
+		return "That joke's is too short.";
+	}
+}
+
+function validateJokeName(name: string) {
+	if (name.length < 10) {
+		return "The joke's name is too short.";
+	}
+}
+
+interface Fields {
+	name?: string;
+	content?: string;
+}
+interface Errors {
+	fieldErrors: Fields | null;
+	fields: Fields | null;
+	formError: string | null;
+}
+
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+	const formData = await request.formData();
+
+	const name = formData.get("name");
+	const content = formData.get("content");
+	if (typeof name !== "string" || typeof content !== "string") {
+		return badRequest<Errors>({
+			fieldErrors: null,
+			fields: null,
+			formError: "Form not submitted correctly",
+		});
+	}
+	const fieldErrors = {
+		name: validateJokeName(name),
+		content: validateJokeContent(content),
+	};
+	const fields = { name, content };
+
+	if (Object.values(fieldErrors).some(Boolean)) {
+		return badRequest<Errors>({ fieldErrors, fields, formError: null });
+	}
+	const joke = await db.joke.create({ data: { name, content } });
+	return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJoke() {
+	const data = useActionData<typeof action>();
+	console.log(data);
 	return (
 		<div>
-			<p>Add your own hilarious joke!</p>
+			<p>Add your own hilarious joke.</p>
 			<form method='post'>
-				<label htmlFor='jokeTitle'>
-					Joke Title
-					<br />
-					<input type='text' name='jokeTitle' />
-				</label>
-				<br />
-				<br />
-				<label htmlFor='jokeContent'>
-					Joke Content
-					<br />
-					<textarea rows={10} cols={100} name='jokeContent' />
-				</label>
-				<br />
-				<button type='submit'>Add new Joke</button>
+				<div>
+					<label>
+						Name:{" "}
+						<input
+							name='name'
+							defaultValue={data?.fields?.name}
+							aria-invalid={Boolean(data?.fieldErrors?.name)}
+							type='text'
+							aria-errormessage={
+								data?.fieldErrors?.name
+									? "name-error"
+									: undefined
+							}
+						/>
+					</label>
+					{data?.fieldErrors?.name ? (
+						<p
+							className='form-validation-error'
+							id='name-error'
+							role='alert'>
+							{data.fieldErrors.name}
+						</p>
+					) : (
+						""
+					)}
+				</div>
+				<div>
+					<label htmlFor='content'>
+						Content:{" "}
+						<textarea
+							name='content'
+							defaultValue={data?.fields?.content}
+							aria-invalid={Boolean(data?.fieldErrors?.content)}
+							aria-errormessage={
+								data?.fieldErrors?.content
+									? "content-error"
+									: undefined
+							}
+						/>
+					</label>
+					{data?.fieldErrors?.content ? (
+						<p
+							className='form-validation-error'
+							role='alert'
+							id='name-error'>
+							{data.fieldErrors.content}
+						</p>
+					) : (
+						""
+					)}
+				</div>
+				<div>
+					<button type='submit' className='button'>
+						Add
+					</button>
+				</div>
 			</form>
 		</div>
 	);
