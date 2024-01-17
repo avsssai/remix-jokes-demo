495b8d5b87beb20d805b7aa7f744c3ced75dadff
Siva Addanki <34810623+avsssai@users.noreply.github.com>
2024-01-17 23:23:13 +0530

    auth flow

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,11 +1,77 @@
-import { useSearchParams } from "@remix-run/react";
-import { Link } from "react-router-dom";
+import type { ActionFunctionArgs, LinksFunction } from "@remix-run/node";
+import {
+	Link,
+	redirect,
+	useActionData,
+	useSearchParams,
+} from "@remix-run/react";
+
+import stylesUrl from "~/styles/login.css";
+import { badRequest } from "~/utils/request.server";
+
+interface Fields {
+	username?: string;
+	password?: string;
+}
+
+interface Errors {
+	fieldErrors: Fields | null;
+	fields: Fields | null;
+	formError: string | null;
+}
+
+export const links: LinksFunction = () => [
+	{ rel: "stylesheet", href: stylesUrl },
+];
+
+const validateUsername = (content: string) => {
+	if (content.length < 3) {
+		return "That username is too short.";
+	}
+};
+
+const validatePassword = (content: string) => {
+	if (content.length < 6) {
+		return "That password is too short, pls choose a stronger password.";
+	}
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+	const formData = await request.formData();
+	const username = formData.get("username");
+	const password = formData.get("password");
+	// rule out the case where the formData is not of type string
+	if (typeof username !== "string" || typeof password !== "string") {
+		return badRequest<Errors>({
+			fieldErrors: null,
+			fields: null,
+			formError: "Form not submitted correctly.",
+		});
+	}
+
+	// form validation
+	const fieldErrors: Fields = {
+		username: validateUsername(username),
+		password: validatePassword(password),
+	};
+
+	const fields: Fields = { username, password };
+
+	if (Object.values(fieldErrors).some(Boolean)) {
+		return badRequest<Errors>({ fieldErrors, fields, formError: null });
+	}
+
+	return redirect("/jokes");
+};
 
 export default function Login() {
 	const [searchParams] = useSearchParams();
+	const data = useActionData();
+
+	console.log(data, "data");
 	return (
 		<div className='container'>
-			<div className='content'>
+			<div className='content' data-light=''>
 				<h1>Login</h1>
 				<form method='post'>
 					<input
@@ -21,7 +87,7 @@ export default function Login() {
 								name='loginType'
 								value='login'
 								defaultChecked
-							/>
+							/>{" "}
 							Login
 						</label>
 						<label>
@@ -29,7 +95,7 @@ export default function Login() {
 								type='radio'
 								name='loginType'
 								value='register'
-							/>
+							/>{" "}
 							Register
 						</label>
 					</fieldset>
@@ -37,16 +103,16 @@ export default function Login() {
 						<label htmlFor='username-input'>Username</label>
 						<input
 							type='text'
-							name='username'
 							id='username-input'
+							name='username'
 						/>
 					</div>
 					<div>
-						<label htmlFor='username-password'>Password</label>
+						<label htmlFor='password-input'>Password</label>
 						<input
-							type='password'
-							name='password'
 							id='password-input'
+							name='password'
+							type='password'
 						/>
 					</div>
 					<button type='submit' className='button'>
